0058a746c520cced801a146706aa1afd1d36c0f4
Vlad <kurguzov74@gmail.com>
2023-01-28 17:51:48 +0300

    Issue start (#1)
    * СТАРТ ПРИЛОЖЕНИЯ - кодСтаил - tslint, tslint-eslint-rules, editorconfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+/* Стартовы компонент */
+
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.scss' ],
 })
 export class AppComponent {
-  title = 'competitions';
+    public title = 'competitions';
 }
