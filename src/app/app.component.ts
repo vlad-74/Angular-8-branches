1b11dcff0a5e54904551b50e7f7adb303b0d536a
Vlad <kurguzov74@gmail.com>
2023-01-28 21:00:52 +0300

    СТАРТ2 - структура папок, helper, developerInfo (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 /* Стартовы компонент */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HelperService } from './helper/helper.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     public title = 'competitions';
+
+    public constructor(
+        private _h: HelperService,
+    ) {
+    }
+
+    public ngOnInit() {
+        console.log('---', this._h.screen.checkScreen());
+    }
 }
