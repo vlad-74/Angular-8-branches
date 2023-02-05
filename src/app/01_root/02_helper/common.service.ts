66b1f42223e9fe29ae174d07823210fbc1f33d88
Vladimir Kurguzov <kurguzov74@gmail.com>
2023-02-05 08:02:06 +0300

    edit

--- src/app/01_root/02_helper/common.service.ts
+++ src/app/01_root/02_helper/common.service.ts
@@ -24,7 +24,7 @@ export class CommonService {
     public log(arg) {
         if (!environment.production) {
             this.logСоunt++;
-            console.log('==log-' + this.logСоunt, ...arg);
+            console.log('log ====================================================== ' + this.logСоunt, ...arg);
         }
     }
 }
