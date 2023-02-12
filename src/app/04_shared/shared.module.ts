bcd80f4a162543df4b18937a43e9cdc7adfd5f7e
Vladimir Kurguzov <kurguzov74@gmail.com>
2023-02-12 19:38:20 +0300

    Интернализация

--- src/app/04_shared/shared.module.ts
+++ src/app/04_shared/shared.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
     declarations: [
@@ -13,6 +23,14 @@ import { WrapperComponent } from './components/wrapper/wrapper.component';
         FormsModule,
         RouterModule,
         ReactiveFormsModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }),
     ],
 })
 export class SharedModule {
--- src/app/07_developer-info/test/test.component.ts
+++ src/app/07_developer-info/test/test.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { HelperService } from '@helper/helper.service';
 import { ThemeService } from '@state/theme.service';
 import { AppSnapshotService } from '@state/app-snapshot.service';
-import { takeUntil } from 'rxjs/operators';
+
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'test',
@@ -13,8 +15,10 @@ import { takeUntil } from 'rxjs/operators';
 export class TestComponent implements OnInit, OnDestroy {
     private readonly destroyed$ = new Subject();
     private currentAppSnapshot;
+    public name = 'Translation';
 
     public constructor(
+        public translate: TranslateService,
         private _h: HelperService,
         private _theme: ThemeService,
         private _snapShot: AppSnapshotService,
@@ -23,6 +27,16 @@ export class TestComponent implements OnInit, OnDestroy {
 
     /* Подписывемся на appSnapshot$*/
     public ngOnInit() {
+        this.translate.addLangs(['en', 'fr', 'vi']);
+        this.translate.setDefaultLang('en');
+
+        const browserLang = this.translate.getBrowserLang();
+
+        this.translate.use(browserLang.match(/en|fr|vi/) ? browserLang : 'en');
+
+        console.log(browserLang);
+        // =========================================================
+
         this._snapShot.appSnapshot$
             .pipe(takeUntil(this.destroyed$))
             .subscribe(
